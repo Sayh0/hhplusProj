143df2145e8b1ed5fe762e8aafd735e351e511bf
Sayh0 <michaelyoon0948@gmail.com>
2025-10-01 02:19:23 +0900

    [feat]add : ProductMapper 및 ProductService 구현, 테스트 추가

--- src/main/java/kr/hhplus/be/server/product/service/ProductService.java
+++ src/main/java/kr/hhplus/be/server/product/service/ProductService.java
@@ -1,16 +1,26 @@
 package kr.hhplus.be.server.product.service;
 
+import kr.hhplus.be.server.product.mapper.ProductMapper;
 import kr.hhplus.be.server.product.vo.ProductVo;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ProductService {
 
+    private final ProductMapper productMapper;
+
+    public ProductService(ProductMapper productMapper) {
+        this.productMapper = productMapper;
+    }
+
     public List<ProductVo> getAllProducts() {
-        // TODO: TDD 단계별로 구현 예정
-        return null;
+        // // TODO: TDD 단계별로 구현 예정
+        // return null;
+        // TDD Green 단계: 테스트를 통과시키기 위한 최소한의 구현
+        // return new ArrayList<>();
+        // Mapper에 위임하여 실제 데이터를 조회한다
+        return productMapper.findAllProducts();
     }
 }
