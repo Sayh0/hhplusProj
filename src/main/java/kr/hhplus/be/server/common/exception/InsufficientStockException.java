e9ab3dec9da4cd0935cfb26b73f94e7830a00405
Sayh0 <michaelyoon0948@gmail.com>
2025-10-11 03:07:26 +0900

    feat: 전역 예외처리 핸들러 및  예외 클래스 추가

---
+++ src/main/java/kr/hhplus/be/server/common/exception/InsufficientStockException.java
@@ -0,0 +1,15 @@
+package kr.hhplus.be.server.common.exception;
+
+/**
+ * 재고 부족 예외
+ */
+public class InsufficientStockException extends RuntimeException {
+
+    public InsufficientStockException(String message) {
+        super(message);
+    }
+
+    public InsufficientStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
